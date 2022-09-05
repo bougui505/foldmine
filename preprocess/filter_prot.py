c7d7823d4492cfd57396c078ac30ee51f90ab6e3
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-09-05 15:47:34 +0200

    foldseek scop filtering of homologs_filtered.txt.gz 	modified:   filter_prot.py

--- preprocess/filter_prot.py
+++ preprocess/filter_prot.py
@@ -3,12 +3,21 @@
 
 import gzip
 
-with open('../data/protein_chains.txt') as protfile:
-    protset = set([e.strip() for e in protfile.readlines()])
+# with open('../data/protein_chains.txt') as protfile:
+#     protset = set([e.strip() for e in protfile.readlines()])
+
+with open('../data/foldseek_list.txt') as protfile:
+    protset = set()
+    for e in protfile.readlines():
+        pdb = e.strip()[5:9].upper()
+        chain = e.strip()[9].upper()
+        if chain == ".":
+            continue
+        protset.add(f'{pdb}_{chain}')
 
 with gzip.open('../data/homologs.txt.gz', 'r') as homologs:
     # with open('test.txt') as homologs:
-    with gzip.open('../data/homologs_filtered.txt.gz', 'wt') as outfile:
+    with gzip.open('../data/homologs_foldseek.txt.gz', 'wt') as outfile:
         for line in homologs.readlines():
             line = line.decode()
             pdbchain = line.split()[0].upper()
