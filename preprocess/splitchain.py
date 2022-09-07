4790ca22fed78b3d6feb3a88a3f1898965372a6f
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-09-07 10:03:48 +0200

    new file:   splitchain.py

---
+++ preprocess/splitchain.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+# -*- coding: UTF8 -*-
+
+#############################################################################
+# Author: Guillaume Bouvier -- guillaume.bouvier@pasteur.fr                 #
+# https://research.pasteur.fr/en/member/guillaume-bouvier/                  #
+# Copyright (c) 2021 Institut Pasteur                                       #
+#                 				                            #
+#                                                                           #
+#  Redistribution and use in source and binary forms, with or without       #
+#  modification, are permitted provided that the following conditions       #
+#  are met:                                                                 #
+#                                                                           #
+#  1. Redistributions of source code must retain the above copyright        #
+#  notice, this list of conditions and the following disclaimer.            #
+#  2. Redistributions in binary form must reproduce the above copyright     #
+#  notice, this list of conditions and the following disclaimer in the      #
+#  documentation and/or other materials provided with the distribution.     #
+#  3. Neither the name of the copyright holder nor the names of its         #
+#  contributors may be used to endorse or promote products derived from     #
+#  this software without specific prior written permission.                 #
+#                                                                           #
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS      #
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT        #
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR    #
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT     #
+#  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,   #
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT         #
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,    #
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY    #
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT      #
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE    #
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.     #
+#                                                                           #
+#  This program is free software: you can redistribute it and/or modify     #
+#                                                                           #
+#############################################################################
+
+import torch
+import glob
+from pymol import cmd
+import os
+
+
+def collate_fn(batch):
+    return batch
+
+
+class Splitter(torch.utils.data.Dataset):
+    """
+    Load pdb files from a PDB database and return coordinates
+    See: ~/source/misc/shell/updatePDB.sh to download the PDB
+    """
+    def __init__(self, outdir='pdb_chainsplit'):
+        self.list_IDs = glob.glob('**/*.mmtf')
+        self.outdir = outdir
+        # os.mkdir(outdir)
+        cmd.reinitialize()
+
+    def __len__(self):
+        return len(self.list_IDs)
+
+    def __getitem__(self, index):
+        pdbfile = self.list_IDs[index]
+        pdbcode = os.path.splitext(os.path.basename(pdbfile))[0]
+        dirname = f'{self.outdir}/{pdbcode[1:3]}'
+        os.makedirs(dirname, exist_ok=True)
+        ls = os.listdir(dirname)
+        for name in ls:
+            if name.startswith(pdbcode):
+                return
+
+        pymolname = index
+        cmd.load(filename=pdbfile, object=pymolname)
+        chains = cmd.get_chains(f'{pymolname} and polymer.protein')
+        for chain in chains:
+            fn = f'{dirname}/{pdbcode}_{chain}.pdb'
+            print(fn)
+            # cmd.save(fn, selection=f'{pymolname} and polymer.protein and chain {chain}')
+        cmd.delete(pymolname)
+
+
+if __name__ == '__main__':
+    import sys
+    import argparse
+    import doctest
+    # argparse.ArgumentParser(prog=None, usage=None, description=None, epilog=None, parents=[], formatter_class=argparse.HelpFormatter, prefix_chars='-', fromfile_prefix_chars=None, argument_default=None, conflict_handler='error', add_help=True, allow_abbrev=True, exit_on_error=True)
+    parser = argparse.ArgumentParser(description='')
+    # parser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
+    parser.add_argument('--test', help='Test the code', action='store_true')
+    args = parser.parse_args()
+
+    splitter = Splitter(outdir='pdb_mmtf_chainsplit')
+    splitter = torch.utils.data.DataLoader(splitter, num_workers=os.cpu_count(), collate_fn=lambda x: x)
+    for _ in splitter:
+        pass
