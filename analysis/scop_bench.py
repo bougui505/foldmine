cc220a4364d6f4445acc610176771aa0dddf7084
Guillaume Bouvier <guillaume.bouvier@pasteur.fr>
2022-10-14 09:44:16 +0200

    renamed:    foldseek_scop_bench.py -> scop_bench.py

--- analysis/scop_bench.py
+++ analysis/scop_bench.py
@@ -39,130 +39,134 @@ import os
 import numpy as np
 import pickle
 from tqdm import tqdm
-
-SCOPFILENAME = 'data/dir.des.scope.2.01-stable.txt'
-FOLDSEEKRESULTS = 'data/alignResults/rawoutput/foldseekaln'
-FOLDSEEKRESULTS_PICKLE = 'data/foldseekresults.pickle'
+import matplotlib.pyplot as plt
 
 
-def parse_scop(scopfilename=SCOPFILENAME):
-    """
-    >>> scop_dict = parse_scop()
-    >>> scop_dict
-    {'d1ux8a_': 'a.1.1.1', 'd1dlwa_': 'a.1.1.1', 'd1uvya_': 'a.1.1.1', ...
+class Bench(object):
 
-    Can retrieve the scop class of a system:
-    >>> scop_dict['d1a1xa_']
-    'b.63.1.1'
-    """
-    data = np.genfromtxt(scopfilename, usecols=(2, 3), dtype=str)
-    sel = (data[:, 1] != '-')
-    data = data[sel]
-    lookupdict = dict(zip(data[:, 1], data[:, 0]))
-    return lookupdict
+    def __init__(self,
+                 scopfilename='data/dir.des.scope.2.01-stable.txt',
+                 results='data/foldseekaln.gz',
+                 results_pickle='data/foldseekresults.pickle'):
+        """
+        >>> bench = Bench()
+        >>> bench.get_scop('d1a1xa_')
+        ['b', '63', '1', '1']
+        >>> query, neighbors = bench.get_result('d1a1xa_')
+        >>> query
+        ['b', '63', '1', '1']
+        >>> neighbors
+        [['b', '63', '1', '1'], ['b', '60', '1', '1'], ['b', '45', '1', '0'], ...
 
+        Get the sensitivity at the family level
+        >>> bench.get_sensitivity('d1mkya2')
+        0.5428571428571428
 
-SCOP_DICT = parse_scop()
+        Get the sensitivity at the superfamily level
+        >>> bench.get_sensitivity('d1mkya2', level=2)
+        0.0036153289949385392
 
-
-def parse_foldseek_results(foldseek_results=FOLDSEEKRESULTS, max_rows=None, outfilename=FOLDSEEKRESULTS_PICKLE):
-    """
-    >>> foldseek_res = parse_foldseek_results(max_rows=100000, outfilename='test.pickle')
-    >>> foldseek_res
-    {'d1a1xa_': array(['d1a1xa_', 'd1jsga_', 'd3saoa_', ...
-
-    foldseek_res retrieve sorted neighbors for a given structure
-    >>> foldseek_res['d1a1xa_']
-    array(['d1a1xa_', 'd1jsga_', 'd3saoa_', ..., 'd1mkna_', 'd1vkza3',
-           'd1vd4a_'], dtype='<U7')
-    """
-    data = np.genfromtxt(foldseek_results, usecols=(0, 1), dtype=str, max_rows=max_rows)
-    p1 = data[:, 0]
-    p2 = data[:, 1]
-    inds = np.where(p1[:-1] != p1[1:])[0] + 1
-    p1 = np.split(p1, inds)
-    p2 = np.split(p2, inds)
-    p1 = [e[0] for e in p1]
-    out = dict(zip(p1, p2))
-    pickle.dump(out, open(outfilename, 'wb'))
-    return out
-
-
-if os.path.exists(FOLDSEEKRESULTS_PICKLE):
-    FOLDSEEKRESULTS_DICT = pickle.load(open(FOLDSEEKRESULTS_PICKLE, 'rb'))
-else:
-    FOLDSEEKRESULTS_DICT = parse_foldseek_results()
-
-
-def get_scop(structure, scop_dict=SCOP_DICT):
-    """
-    Returns: [Class, Fold, Superfamily, Family]
-    >>> get_scop('d1a1xa_')
-    ['b', '63', '1', '1']
-    """
-    scop = scop_dict[structure]
-    scop = scop.split('.')
-    return scop
-
-
-def get_foldseek_result(structure, foldseekresults_dict=FOLDSEEKRESULTS_DICT):
-    """
-    >>> query, neighbors = get_foldseek_result('d1a1xa_')
-    >>> query
-    ['b', '63', '1', '1']
-    >>> neighbors
-    [['b', '63', '1', '1'], ['b', '60', '1', '1'], ['b', '45', '1', '0'], ...
-    """
-    neighbors = list(foldseekresults_dict[structure])
-    neighbors.remove(structure)
-    query = get_scop(structure)
-    neighbors = [get_scop(e) for e in neighbors]
-    return query, neighbors
-
-
-def get_sensitivity(structure, level=3):
-    """
-    Family: level=3
-    Superfamily: level=2
-    Fold: level=1
-    >>> get_sensitivity('d1mkya2')
-    0.5428571428571428
-    >>> get_sensitivity('d1mkya2', level=2)
-    0.0036153289949385392
-    >>> get_sensitivity('d1mkya2', level=1)
-    -1
-    """
-    query, neighbors = get_foldseek_result(structure)
-    upFP = []
-    for neig in neighbors:
-        if neig[1] != query[1] or neig[0] != query[0]:
-            break
-        upFP.append(neig)
-
-    def filter(scop, level, target):
-        scoptest = scop[level] == target[level]
-        if level == 3:
-            return scoptest
+        Get the sensitivity at the fold level
+        >>> bench.get_sensitivity('d1mkya2', level=1)
+        -1
+        """
+        self.scopfilename = scopfilename
+        self.results = results
+        self.results_pickle = results_pickle
+        self.scop_dict = self.__parse_scop__()
+        if os.path.exists(self.results_pickle):
+            self.results_dict = pickle.load(open(self.results_pickle, 'rb'))
         else:
-            scoptest = (scoptest and scop[level + 1] != target[level + 1])
-            return scoptest
+            self.results_dict = self.__parse_results__()
 
-    upFP = [e for e in upFP if filter(e, level, query)]
-    num = len(upFP)
-    allhits = [e for e in neighbors if filter(e, level, query)]
-    den = len(allhits)
-    if den == 0:
-        return -1
-    return num / den
+    def __parse_scop__(self):
+        """
+        """
+        data = np.genfromtxt(self.scopfilename, usecols=(2, 3), dtype=str)
+        sel = (data[:, 1] != '-')
+        data = data[sel]
+        lookupdict = dict(zip(data[:, 1], data[:, 0]))
+        return lookupdict
 
+    def __parse_results__(self, max_rows=None):
+        """
+        """
+        data = np.genfromtxt(self.results, usecols=(0, 1), dtype=str, max_rows=max_rows)
+        p1 = data[:, 0]
+        p2 = data[:, 1]
+        inds = np.where(p1[:-1] != p1[1:])[0] + 1
+        p1 = np.split(p1, inds)
+        p2 = np.split(p2, inds)
+        p1 = [e[0] for e in p1]
+        out = dict(zip(p1, p2))
+        pickle.dump(out, open(self.results_pickle, 'wb'))
+        return out
 
-def all_sensitivity(foldseekresults_dict=FOLDSEEKRESULTS_DICT, level=3):
-    all_s = []
-    for structure in tqdm(foldseekresults_dict):
-        s = get_sensitivity(structure, level=level)
-        if s != -1:
-            all_s.append(s)
-    return all_s
+    def get_scop(self, structure):
+        """
+        Returns: [Class, Fold, Superfamily, Family]
+        """
+        scop = self.scop_dict[structure]
+        scop = scop.split('.')
+        return scop
+
+    def get_result(self, structure):
+        """
+        """
+        neighbors = list(self.results_dict[structure])
+        neighbors.remove(structure)
+        query = self.get_scop(structure)
+        neighbors = [self.get_scop(e) for e in neighbors]
+        return query, neighbors
+
+    def get_sensitivity(self, structure, level=3):
+        """
+        Family: level=3
+        Superfamily: level=2
+        Fold: level=1
+        """
+        query, neighbors = self.get_result(structure)
+        upFP = []
+        for neig in neighbors:
+            if neig[1] != query[1] or neig[0] != query[0]:
+                break
+            upFP.append(neig)
+
+        def filter(scop, level, target):
+            scoptest = scop[level] == target[level]
+            if level == 3:
+                return scoptest
+            else:
+                scoptest = (scoptest and scop[level + 1] != target[level + 1])
+                return scoptest
+
+        upFP = [e for e in upFP if filter(e, level, query)]
+        num = len(upFP)
+        allhits = [e for e in neighbors if filter(e, level, query)]
+        den = len(allhits)
+        if den == 0:
+            return -1
+        return num / den
+
+    def all_sensitivity(self, outfilename, level=3, plotfile=None):
+        all_s = []
+        for structure in tqdm(self.results_dict):
+            s = self.get_sensitivity(structure, level=level)
+            if s != -1:
+                all_s.append(s)
+        np.savetxt(outfilename, all_s)
+        if plotfile is not None:
+            plt.plot(sorted(all_s, reverse=True))
+            plt.xlabel('Fraction of queries')
+            plt.ylabel('Sensitivity up to 1st FP')
+            if level == 3:
+                plt.title('Family')
+            if level == 2:
+                plt.title('Super Family')
+            if level == 1:
+                plt.title('Fold')
+            plt.savefig(plotfile)
+        return all_s
 
 
 def log(msg):
@@ -197,6 +201,7 @@ if __name__ == '__main__':
     parser.add_argument('-a', '--arg1')
     parser.add_argument('--test', help='Test the code', action='store_true')
     parser.add_argument('--func', help='Test only the given function(s)', nargs='+')
+    parser.add_argument('--foldseek', help='Foldseek SCOP bench', action='store_true')
     args = parser.parse_args()
 
     # If log is present log the arguments to the log file:
@@ -213,5 +218,12 @@ if __name__ == '__main__':
                 doctest.run_docstring_examples(f, globals())
         sys.exit()
 
-    all_s = all_sensitivity()
-    np.savetxt('sens.txt', all_s)
+    level = 3
+    os.makedirs('out', exist_ok=True)
+    if args.foldseek:
+        results = 'data/foldseekaln.gz'
+        results_pickle = 'data/foldseekresults.pickle'
+        outbasename = f'out/foldseek_{level}'
+    bench = Bench(results=results, results_pickle=results_pickle)
+    all_s = bench.all_sensitivity(outfilename=outbasename + '.txt', level=level, plotfile=outbasename + '.svg')
+    # np.savetxt('sens.txt', all_s)
